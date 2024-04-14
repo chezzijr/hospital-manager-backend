2a460f4849144b19c3db60f46d867f2d116b041c
chezzijr <nhuthuan0501@gmail.com>
2024-04-14 11:18:22 +0700

    feat: add cors

--- app/src/main/java/org/hospitalmanager/App.java
+++ app/src/main/java/org/hospitalmanager/App.java
@@ -5,11 +5,24 @@ package org.hospitalmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class App {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 }
